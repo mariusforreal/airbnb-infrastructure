89905e320fccf9d6755ca022efe2ebff7b0f6839
Marius Songwa <mariusforreal@gmail.com>
2022-10-29 12:41:27 -0500

    Add files via upload

---
+++ ec2.tf
@@ -0,0 +1,17 @@
+resource "aws_instance" "web" {
+  ami           = var.AMI_ID
+  instance_type = "t3.micro"
+  monitoring = true
+  ebs_optimized = true
+  metadata_options {
+    http_endpoint = "enabled"
+    http_tokens = "required"
+  }
+  root_block_device {
+    encrypted = true
+  }
+
+  tags = {
+    Name = "airbnb-web-server"
+  }
+}
